02da60c30d53091db834d0473b322e3aaa9d98d5
kagion <soegiono1998@gmail.com>
2021-01-19 13:33:57 +0700

    batik app v1
    Signed-off-by: kagion <soegiono1998@gmail.com>

--- app/src/main/java/com/example/batik/Data.kt
+++ app/src/main/java/com/example/batik/Data.kt
@@ -4,4 +4,5 @@ import com.google.firebase.database.Exclude
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
-data class Data(val name: String? = null, val date: String? = null, val asal: String? = null, val description: String? = null, val url: String? = null, @Exclude val key: String? = null)
+data class Data(val name: String? = null, val date: String? = null, val asal: String? = null, val
+description: String? = null, val url: String? = null, @Exclude val key: String? = null)
